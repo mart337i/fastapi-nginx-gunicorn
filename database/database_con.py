7958c1e36fb4a702af4d9a562504857a3ee68b7c
mart337i <mart337i@gmail.com>
2023-10-20 10:22:14 +0100

    [ADD] Chnge config + setup db

--- database/database_con.py
+++ database/database_con.py
@@ -2,23 +2,66 @@
 import mariadb
 import sys
 
-class MariaDB():
-    __name__ = "Maria Db"
-    __description__ = "Maria db connection"
-
+class MariaDB:
+    def __init__(self):
+        self.setup()  # Automatically call setup when an instance is created
 
     def get_curser(self):
-        # Connect to MariaDB Platform
         try:
+            print("Starting connection atempt")
             conn = mariadb.connect(
-                user="db_user",
-                password="db_user_passwd",
-                host="192.0.2.1",
+                user="pi",
+                password="Vds79bzw-",
+                host="localhost",
                 port=3306,
-                database="employees"
+                database="facility"
             )
-            # Get Cursor
+            print(f"connection : {conn}")
             return conn.cursor()
         except mariadb.Error as e:
             print(f"Error connecting to MariaDB Platform: {e}")
             sys.exit(-1)
+
+
+
+    def execute(self,cursor,query):
+        """
+            Method to exacute any query.
+            *self
+            *curser db curser 
+            *query 
+            
+        """
+        try:
+            cursor.execute(query)
+        except mariadb.Error as e:
+            print(f"Error: {e}")
+
+    def setup(self):
+        """ Init method for init query
+        
+        """
+        _cr = self.get_curser()
+        print(_cr.connection)  # Fixed this line by printing _cr.connection instead of res.connection
+
+
+    def innit_query ():
+        return """
+            USE facility; 
+            CREATE TABLE facility (
+                ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                DESC CHAR,
+            );
+            CREATE TABLE building (
+                ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            ) ;
+            CREATE TABLE pollution_sensor (
+                ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            
+            );
+            CREATE TABLE temperature_humidity_sensor (
+                ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            
+            );
+
+        """
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # Create hello world FastAPI app
 from fastapi import FastAPI
-from database import database_con 
+from database import database_con
+
+database = database_con.MariaDB
+cr = database.get_curser
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    
     return {"message": "It's working!"}
 
-@app.get("/facility/{id}")
+@app.get("/facility")
 def get_facility(id : int ):
-    return {"Facility" : ""}
+    res = database.execute(cr,get_facility_query)
+    return {"Facility" : f"{res}"}
     
+def get_facility_query():
+    return """
+        Select * facility;
+    """
